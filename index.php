b22ac329c6b27aa6e194acb8dc201a1b142c6958
Jermain Jennings <jermainejennings05@gmail.com>
2017-11-08 16:53:21 -0700

    3 sections added with text.

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>Java Script challenge</title>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	</head>
+	<body>
+		<div class="jumbotron jumbotron-fluid">
+			<div class="container">
+				<h1 class="display-3">Fluid jumbotron</h1>
+				</div>
+			</div>
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+		<section>
+			<div class="container-fluid">
+				<div class="p-3 mb-2 bg-secondary text-white">.bg-secondary>
+				<P>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum mattis sagittis.
+					Quisque in lobortis tortor, vel aliquam libero. In ornare, nunc ut placerat aliquam, odio odio venenatis
+					magna, sed vestibulum urna metus id mauris. Duis ac consequat libero, in aliquam lacus.
+					Aliquam nec tincidunt ligula, et condimentum erat. Maecenas accumsan justo sed nulla ultrices, in
+					venenatis leo ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</P>
+				</div>
+			</div>
+		</section>
+
+		<section>
+			<div class="container-fluid">
+				<div class="p-3 mb-2 bg-secondary text-white">.bg-secondary>
+				<p>Donec et ultricies magna, nec varius mi. Sed volutpat tellus ac arcu mollis vestibulum. Aenean tempus
+					porttitor vulputate. Maecenas et tellus vel dolor ultricies maximus in sit amet magna. Vestibulum ante
+					ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vestibulum ante ipsum primis in
+					faucibus orci luctus et ultrices posuere cubilia Curae; Donec libero diam, interdum ac leo in, molestie
+					egestas risus. Ut ullamcorper euismod massa vitae congue. Donec non fringilla purus, eget cursus neque.
+					Suspendisse ut enim ut erat convallis iaculis. Praesent sit amet orci sagittis, tincidunt ex quis,
+					tristique tortor. Curabitur hendrerit eget neque ac facilisis. Sed lorem leo, sodales at tempus vitae,
+					congue ac urna. Sed finibus aliquam luctus.</p>
+				</div>
+			</div>
+		</section>
+
+		<section>
+			<div class="container-fluid">
+				<div class="p-3 mb-2 bg-secondary text-white">.bg-secondary>
+				<p>Pellentesque est odio, tempus sed pulvinar a, luctus id nibh. Fusce tristique iaculis lacinia.
+					Etiam dignissim lectus turpis, in dictum erat elementum eu. Curabitur venenatis metus leo, vitae
+					scelerisque elit tincidunt sed. Nullam et lacus vel erat porttitor interdum. Nulla egestas sapien purus,
+					non aliquet sem malesuada at. Aliquam erat volutpat. Nunc urna tellus, dapibus id metus non, sollicitudin
+					malesuada nunc. Donec in mollis ligula. Proin finibus ut neque eget dictum. Phasellus facilisis sem sit
+					amet diam viverra rhoncus.</p>
+				</div>
+			</div>
+		</section>
+	</body>
+</html>
